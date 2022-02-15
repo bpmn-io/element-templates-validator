1e79ecbd0e6c071d8635ffdda9ea8e96d207433d
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-02-15 12:51:10 +0100

    feat(ZeebeValidator): add zeebe-element-templates-json-schema validation
    Closes #5

--- tasks/createStandaloneValidator.js
+++ tasks/createStandaloneValidator.js
@@ -4,10 +4,28 @@ import { join as pathJoin, dirname } from 'path';
 
 import validate, { ajv } from '../lib/validate';
 
+import validateZeebe, { ajv as zeebeAjv } from '../lib/validateZeebe';
+
 
 export function createStandaloneValidator() {
   const code = standaloneCode(ajv, validate);
   const filePath = pathJoin('tmp', 'standaloneValidator.js');
+
+  try {
+    mkdir(dirname(filePath));
+  } catch {
+
+    // directory may already exist
+  }
+
+  writeFile(filePath, code);
+
+  return filePath;
+}
+
+export function createStandaloneZeebeValidator() {
+  const code = standaloneCode(zeebeAjv, validateZeebe);
+  const filePath = pathJoin('tmp', 'standaloneZeebeValidator.js');
 
   try {
     mkdir(dirname(filePath));
--- test/distro/distroSpec.js
+++ test/distro/distroSpec.js
@@ -8,11 +8,21 @@ describe('validate module', function() {
   it('should expose CJS bundle', function() {
 
     // given
-    const { validate, validateAll, getSchemaVersion } = require('../../dist');
+    const {
+      validate,
+      validateAll,
+      getSchemaVersion,
+      validateZeebe,
+      validateAllZeebe,
+      getZeebeSchemaVersion
+    } = require('../../dist');
 
     // then
     expect(validate).to.exist;
     expect(validateAll).to.exist;
     expect(getSchemaVersion).to.exist;
+    expect(validateZeebe).to.exist;
+    expect(validateAllZeebe).to.exist;
+    expect(getZeebeSchemaVersion).to.exist;
   });
 });
