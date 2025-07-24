89cffb12c4d00d1b7de9d45d57827bea7ca154d0
Anton von Weltzien <anton.weltzien@camunda.com>
2025-07-24 16:41:51 +0200

    test: add `zeebe:assignmentDefinition` test

--- test/spec/validationSpec.js
+++ test/spec/validationSpec.js
@@ -252,7 +252,7 @@ describe('Validator', function() {
           message: 'must provide choices=[] with "Dropdown" type'
         },
         {
-          message: 'invalid property.binding type "zeebe:taskDefinition:foo"; must be any of { property, zeebe:taskDefinition:type, zeebe:input, zeebe:output, zeebe:property, zeebe:taskHeader, bpmn:Message#property, bpmn:Message#zeebe:subscription#property, zeebe:taskDefinition, zeebe:calledElement, zeebe:linkedResource, zeebe:userTask, zeebe:formDefinition, zeebe:calledDecision, zeebe:script }'
+          message: 'invalid property.binding type "zeebe:taskDefinition:foo"; must be any of { property, zeebe:taskDefinition:type, zeebe:input, zeebe:output, zeebe:property, zeebe:taskHeader, bpmn:Message#property, bpmn:Message#zeebe:subscription#property, zeebe:taskDefinition, zeebe:calledElement, zeebe:linkedResource, zeebe:userTask, zeebe:formDefinition, zeebe:calledDecision, zeebe:script, zeebe:assignmentDefinition }'
         },
         {
           message: 'property.binding "zeebe:taskHeader" requires key'
@@ -677,6 +677,45 @@ describe('Validator', function() {
 
       // given
       const samples = require('../fixtures/category-broken.json');
+
+      // when
+      const {
+        valid,
+        results
+      } = validateAllZeebe(samples);
+
+      // then
+      expect(valid).to.be.false;
+      expect(results.every(r => !r.valid)).to.be.true;
+      expect(results.map(r => r.object)).to.eql(samples);
+    });
+
+
+    it('should validate assignmentDefinition templates', function() {
+
+      // given
+      const samples = require('../fixtures/assignment-definition.json');
+
+      // when
+      const {
+        valid,
+        results
+      } = validateAllZeebe(samples);
+
+      // then
+      expect(valid).to.be.true;
+      expect(results.length).to.eql(samples.length);
+
+      expect(results.every(r => r.valid)).to.be.true;
+
+      expect(results.map(r => r.object)).to.eql(samples);
+    });
+
+
+    it('should validate assignmentDefinition templates with errors', function() {
+
+      // given
+      const samples = require('../fixtures/assignment-definition-broken.json');
 
       // when
       const {
