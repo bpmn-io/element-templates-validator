86c75d5e5e4b2f1db71c842fa3c9ca8919eaccf6
Anton von Weltzien <anton.weltzien@camunda.com>
2025-11-04 15:49:33 +0100

    feat: support signal events
    Related to https://github.com/camunda/camunda-modeler/issues/5381
    
    deps: update to `@camunda/zeebe-element-templates-json-schema@0.33.0`

--- test/spec/validationSpec.js
+++ test/spec/validationSpec.js
@@ -334,7 +334,7 @@ describe('Validator', function() {
           message: 'must provide choices=[] with "Dropdown" type'
         },
         {
-          message: 'invalid property.binding type "zeebe:taskDefinition:foo"; must be any of { property, zeebe:taskDefinition:type, zeebe:input, zeebe:output, zeebe:property, zeebe:taskHeader, bpmn:Message#property, bpmn:Message#zeebe:subscription#property, zeebe:taskDefinition, zeebe:calledElement, zeebe:linkedResource, zeebe:userTask, zeebe:formDefinition, zeebe:calledDecision, zeebe:script, zeebe:assignmentDefinition, zeebe:priorityDefinition, zeebe:adHoc, zeebe:taskSchedule }'
+          message: 'invalid property.binding type "zeebe:taskDefinition:foo"; must be any of { property, zeebe:taskDefinition:type, zeebe:input, zeebe:output, zeebe:property, zeebe:taskHeader, bpmn:Message#property, bpmn:Message#zeebe:subscription#property, zeebe:taskDefinition, zeebe:calledElement, zeebe:linkedResource, zeebe:userTask, zeebe:formDefinition, zeebe:calledDecision, zeebe:script, zeebe:assignmentDefinition, zeebe:priorityDefinition, zeebe:adHoc, zeebe:taskSchedule, bpmn:Signal#property }'
         },
         {
           message: 'property.binding "zeebe:taskHeader" requires key'
@@ -585,6 +585,45 @@ describe('Validator', function() {
 
       // given
       const samples = require('../fixtures/message-broken.json');
+
+      // when
+      const {
+        valid,
+        results
+      } = validateAllZeebe(samples);
+
+      // then
+      expect(valid).to.be.false;
+      expect(results.every(r => !r.valid)).to.be.true;
+      expect(results.map(r => r.object)).to.eql(samples);
+    });
+
+
+    it('should validate signal templates', function() {
+
+      // given
+      const samples = require('../fixtures/signal.json');
+
+      // when
+      const {
+        valid,
+        results
+      } = validateAllZeebe(samples);
+
+      // then
+      expect(valid).to.be.true;
+      expect(results.length).to.eql(samples.length);
+
+      expect(results.every(r => r.valid)).to.be.true;
+
+      expect(results.map(r => r.object)).to.eql(samples);
+    });
+
+
+    it('should validate signal templates with errors', function() {
+
+      // given
+      const samples = require('../fixtures/signal-broken.json');
 
       // when
       const {
