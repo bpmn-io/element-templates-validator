66a1f844d35aecba3341f0f8441ca8770ff13f32
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2025-11-05 13:32:01 +0100

    chore: use rollup with ESM

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import alias from '@rollup/plugin-alias';
 
-import pkg from './package.json';
+import pkg from './package.json' with { type: 'json' };
 import {
   createStandaloneValidator,
   createStandaloneZeebeValidator
